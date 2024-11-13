48cab1fabe35e527dbaca6e634ed180aa3e86a61
Sergey_Chalyi <supersupergray@gmail.com>
2024-11-13 18:53:44 +0200

    Created serializer and api end-points witch are connected with created model Event

--- base/base/urls.py
+++ base/base/urls.py
@@ -17,6 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from events.views import EventsAPIViews, EventDetailAPIViews
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('api/v1/events/', EventsAPIViews.as_view()),
+    path('api/v1/events/<int:pk>/', EventDetailAPIViews.as_view()),
 ]
--- base/events/views.py
+++ base/events/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from rest_framework import generics, permissions
+from events.models import Event
+from events.serializers import EventSerializer
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 
-# Create your views here.
+
+class EventsAPIViews(generics.ListCreateAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+
+
+
+class EventDetailAPIViews(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+
